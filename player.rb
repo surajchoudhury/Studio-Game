e3deb0bdf503e24e68a4996184951ff9cfa285f8
sunny <sunnychoudhury007@gmail.com>
2020-11-17 17:43:49 +0530

    refactor playable

--- player.rb
+++ player.rb
@@ -2,8 +2,7 @@ require_relative 'playable'
 
 class Player
   include Playable
-  attr_reader :health
-  attr_accessor :name
+  attr_accessor :name, :health
 
   def initialize(name, health=150)
      @name = name.capitalize
