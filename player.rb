5752b1fcef2382021218ec2a82b1ff563567a22d
sunny <sunnychoudhury007@gmail.com>
2020-11-17 14:50:59 +0530

    add player

---
+++ player.rb
@@ -0,0 +1,57 @@
+class Player
+  attr_reader :health
+  attr_accessor :name
+
+  def initialize(name, health=150)
+     @name = name.capitalize
+     @health = health
+     @found_treasures = Hash.new(0)
+  end
+
+  def blam
+     @health -= 10
+     puts "#{@name} got blammed"
+  end
+
+  def w00t
+     @health += 15
+     puts "#{@name} got w00ted"
+  end
+
+  def strong? 
+   @health > 100
+  end
+
+  def <=>(player)
+   player.score <=> score
+  end
+  
+  def found_treasure(treasure)
+   @found_treasures[treasure.name] += treasure.points
+   puts "#{@name} found a #{treasure.name} worth #{treasure.points} points."
+   puts "#{@name}'s treasures: #{@found_treasures}"
+  end
+
+  def each_found_treasure 
+   @found_treasures.each do |name, points|
+      yield Treasure.new(name, points)
+   end
+  end
+
+  def points
+   @found_treasures.values.reduce(0, :+)
+  end
+
+  def score
+     @health + points
+  end
+
+  def self.from_csv(string)
+   name, health = string.split(',')
+   Player.new(name, Integer(health))
+ end
+
+  def to_s
+     "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
+end
